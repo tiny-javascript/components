df996a6ff8a00f2a1094ca192c71d81ebfb6cd5c
xiaxianlin <dreamtimefly@163.com>
2016-12-07 10:41:30 +0800

    fix

--- app/pages/result/Index.jsx
+++ app/pages/result/Index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 export default class IndexPage extends React.Component {
+    state = {
+        automation: 'Radio Checkbox Form Table Tabs Menu Cascader Progress Select Tooltip Popover Modal Popconfirm message Dropdown Icon Row Col Breadcrumb Collapse Upload Alert Tree',
+        cmdb: 'Input InputNumber Select Checkbox Radio DatePicker Form Table Button Tabs Menu Mention Tag Modal message Affix Grid Upload Tooltip Popover',
+        uem: 'Modal Dropdown Pagination Tabs Button Select Cascader Upload Popover Tooltip message Popconfirm Table Switch',
+        mointor: 'Select Button Icon Row Col Tooltip Spin InputNumber Table Collapse Tag message Form Menu Dropdown Modal QueueAnim Breadcrumb Checkbox DatePicker Radio Timeline BackTop',
+        apm: 'Table Select Input DatePicker Modal Button Tag Checkbox Radio',
+        show: 'Input InputNumber Checkbox message Button Popover Icon Form Select Radio Tooltip Modal Table Alert Pagination Dropdown Menu Tabs Upload Progress ColorPicker ColorGroupPicker',
+        itsm: 'Input Checkbox message Button Popover Row Col Icon Form Select Radio DatePicker notification Tooltip Card Modal Switch Table Tag Alert Pagination Dropdown Menu Tabs TimePicker Cascader Upload Popconfirm Progress',
+        tenant: 'Modal message Button Row Col Form Input Upload Icon Progress Tooltip Select Table Pagination Badge Dropdown Menu Spin Radio Checkbox Popover DatePicker InputNumber Tree QueueAnim'
+    }
     _format() {
-        const data = {
-            automation: 'Radio Checkbox Form Table Tabs Menu Cascader Progress Select Tooltip Popover Modal Popconfirm message Dropdown Icon Row Col Breadcrumb Collapse Upload Alert Tree',
-            cmdb: 'Input InputNumber Select Checkbox Radio DatePicker Form Table Button Tabs Menu Mention Tag Modal message Affix Grid Upload Tooltip Popover',
-            uem: 'Modal Dropdown Pagination Tabs Button Select Cascader Upload Popover Tooltip message Popconfirm Table Switch',
-            mointor: 'Select Button Icon Row Col Tooltip Spin InputNumber Table Collapse Tag message Form Menu Dropdown Modal QueueAnim Breadcrumb Checkbox DatePicker Radio Timeline BackTop',
-            apm: 'Table Select Input DatePicker Modal Button Tag Checkbox Radio',
-            show: 'Input InputNumber Checkbox message Button Popover Icon Form Select Radio Tooltip Modal Table Alert Pagination Dropdown Menu Tabs Upload Progress ColorPicker ColorGroupPicker',
-            itsm: 'Input Checkbox message Button Popover Row Col Icon Form Select Radio DatePicker notification Tooltip Card Modal Switch Table Tag Alert Pagination Dropdown Menu Tabs TimePicker Cascader Upload Popconfirm Progress',
-            tenant: 'Modal message Button Row Col Form Input Upload Icon Progress Tooltip Select Table Pagination Badge Dropdown Menu Spin Radio Checkbox Popover DatePicker InputNumber Tree QueueAnim'
-        }
         let tmp = [];
-        for (let key in data) {
-            const value = data[key];
+        for (let key in this.state) {
+            const value = this.state[key];
             tmp = tmp.concat(value.split(' '));
         }
         tmp = _.uniq(tmp).sort();
